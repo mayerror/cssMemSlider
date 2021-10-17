529acb8323da4ccb52121b43baa2265c2c0f8cd5
Sergey Masiuk <mayerror@gmail.com>
2021-10-17 15:15:23 +0300

    feat: add update caption with animation function (Sun Oct 17 15:15:22 2021)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -4,7 +4,9 @@ const slideWrapper = document.querySelector('.mem-slider__container'),
       slides = document.querySelectorAll('.mem-slider__slide'),
       bulletsAreas = document.querySelectorAll('.mem-slider__bullet-area'),
       bullets = document.querySelectorAll('.mem-slider__bullet'),
+      captions = document.querySelectorAll('.mem-slider__caption'),
       slideWidth = slides[0].offsetWidth;
+      
 let index = 1;
 
 
@@ -20,16 +22,30 @@ function init() {
           index = +item.dataset.order;
         }
         bulletsUpdate();
+        captionUpdate();
       }
     });
   });
 }
 
+// Update active bullet
 function bulletsUpdate() {
   bullets.forEach(bullet => {
     bullet.classList.remove('mem-slider__bullet_active');
   });
   bullets[index-1].classList.add('mem-slider__bullet_active');
+}
+
+// Update active caption with animation
+function captionUpdate() {
+  captions.forEach(caption => {
+    caption.classList.remove('mem-slider__caption_active');
+    caption.classList.remove('mem-slider__caption_trans');
+  });
+  captions[index-1].classList.add('mem-slider__caption_active');
+  setTimeout(() => {
+    captions[index-1].classList.add('mem-slider__caption_trans');
+  }, 0);
 }
 
 init();
