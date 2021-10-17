f0608aa492df9617261c0b7dad55d50864403ae1
Sergey Masiuk <mayerror@gmail.com>
2021-10-17 13:41:41 +0300

    feat: add bullets interactive function (Sun Oct 17 13:41:40 2021)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -2,50 +2,34 @@ console.log("CSS MEM SLIDER");
 
 const slideWrapper = document.querySelector('.mem-slider__container'),
       slides = document.querySelectorAll('.mem-slider__slide'),
-      bullets = document.querySelectorAll('.mem-slider__bullet-area'),
+      bulletsAreas = document.querySelectorAll('.mem-slider__bullet-area'),
+      bullets = document.querySelectorAll('.mem-slider__bullet'),
       slideWidth = slides[0].offsetWidth;
+let index = 1;
 
 
 function init() {
-  // if (slides.length) {
-  //   const firstNode = slides[0].cloneNode(true),
-  //         lastNode = slides[slides.length-1].cloneNode(true);
-
-
-  //   slide_wrapper.appendChild(firstNode);
-  //   // slide_wrapper.insertBefore(lastNode, slides[0]);
-  // }
-  bullets.forEach(item => {
+  bulletsAreas.forEach(item => {
     item.addEventListener('click', e => {
-      switch (item.dataset.order) {
-        case "1":
+      if(+item.dataset.order != index) {
+        if (item.dataset.order == "1") {
           slideWrapper.style.left = `0px`;
           index = 1;
-          break;
-        case "2":
-          slideWrapper.style.left = `-${slideWidth}px`;
-          index = 2;
-          break;
-        case "3":
-          slideWrapper.style.left = `-${2*slideWidth}px`;
-          index = 3;
-          break;
-        case "4":
-          slideWrapper.style.left = `-${3*slideWidth}px`;
-          index = 4;
-          break;
-        default:
-          break;
+        } else {
+          slideWrapper.style.left = `-${(+item.dataset.order - 1)*slideWidth}px`;
+          index = +item.dataset.order;
+        }
+        bulletsUpdate();
       }
-      // if (item.dataset.order = "2") {
-      //   slideWrapper.style.left = `-${slideWidth}px`;
-      // }
     });
   });
 }
 
-function slide() {
-
+function bulletsUpdate() {
+  bullets.forEach(bullet => {
+    bullet.classList.remove('mem-slider__bullet_active');
+  });
+  bullets[index-1].classList.add('mem-slider__bullet_active');
 }
 
 init();
